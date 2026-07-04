b02826656fd74f55ebfc446cbc0e9e3190114b54
Odessa Elie-David <46230255+odessaelie@users.noreply.github.com>
2026-07-04 16:32:30 -0500

    Update homework1.py with conditions and lists
    Added Part 2

--- homework1.py
+++ homework1.py
@@ -1,5 +1,6 @@
 # Homework 1 Template: Getting Started with Your Assistant
 
+# Part 1
 # Create a file named assistant.py and use it to complete the following tasks
 # TODO: Greet the user
 # TODO: Ask for their name, age, and if they are a student
@@ -10,3 +11,8 @@
 
 # Example:
 # name = input("What is your name? ")
+
+# Part 2
+# TODO: Ask how the user's day is and use if/elif/else statements to give the user feedback based on their response.
+# TODO: Ask for 5 hobbies and store them in a list
+# TODO: Show hobbies using slicing (e.g., hobbies[:2])
